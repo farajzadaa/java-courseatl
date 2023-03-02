8f871f3d860efec22bc3e5bda2f163466c1e045f
farajzadaa <farajzada@icloud.com>
2023-03-02 20:52:43 +0400

    initial commit

--- src/main/java/az/atlacademy/hello.java
+++ src/main/java/az/atlacademy/hello.java
@@ -3,5 +3,7 @@ package az.atlacademy;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
     }
 }
