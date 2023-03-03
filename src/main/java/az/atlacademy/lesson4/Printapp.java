070edcbf958a89e1b3acdc15b31bdf1ff4157596
farajzadaa <farajzada@icloud.com>
2023-03-03 19:35:48 +0400

    initial commit

---
+++ src/main/java/az/atlacademy/lesson4/Printapp.java
@@ -0,0 +1,10 @@
+package az.atlacademy.lesson4;
+
+public class Printapp {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.println("Hello, World");
+        System.out.printf("Hello,%s", "World");
+        System.out.printf("Hello, World:%.2f",50F);
+    }
+}
