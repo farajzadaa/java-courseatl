cfd15f219ecc62dbdfcb61142f48abc4222413e9
farajzadaa <farajzada@icloud.com>
2023-03-06 19:52:37 +0400

    initial commit

--- src/main/java/lesson5/Operator.java
+++ src/main/java/lesson5/Operator.java
@@ -2,15 +2,42 @@ package lesson5;
 
 public class Operator {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
+//        int a = 10;
+//        double b = 2;
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        byte c = 5;
+//        float d = 2;
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+//        System.out.println(a % b);
+//        System.out.println(a++);
+//        System.out.println(b--);
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
-        System.out.println(a++);
-        System.out.println(b--);
+
+        // create variables
+        int a = 7, b = 11;
+
+        // value of a and b
+        System.out.println("a is " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b);  // false
+
+        // != operator
+        System.out.println(a != b);  // true
+
+        // > operator
+        System.out.println(a > b);  // false
+
+        // < operator
+        System.out.println(a < b);  // true
+
+        // >= operator
+        System.out.println(a >= b);  // false
+
+        // <= operator
+        System.out.println(a <= b);  // true
     }
 }
