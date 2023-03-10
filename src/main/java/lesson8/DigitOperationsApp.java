2123e7b040d2a7a36eb22fba20998f782362dfbf
farajzadaa <farajzada@icloud.com>
2023-03-10 19:46:39 +0400

    initial commit

--- src/main/java/lesson8/DigitOperationsApp.java
+++ src/main/java/lesson8/DigitOperationsApp.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class DigitOperationsApp {
     public static void main(String[] args) {
+        // 1. input
         int num = new Scanner(System.in).nextInt();
-
+        // 2. process
         int sum = 0;
         int prod = 1;
         int count = 0;
@@ -17,7 +18,7 @@ public class DigitOperationsApp {
             sum += rem;
             prod *= rem;
         }
-
+        // 3. output
         System.out.printf("Sum of digits is: %d\n",sum);
         System.out.printf("Prod of digits is: %d\n",prod);
         System.out.printf("Count of digits is:\n",count);
