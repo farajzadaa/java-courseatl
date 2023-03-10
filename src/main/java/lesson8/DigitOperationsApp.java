6f3fc2cfa94020fe6e68c681ab1871c8c12ba08c
farajzadaa <farajzada@icloud.com>
2023-03-10 19:49:44 +0400

    initial commit

--- src/main/java/lesson8/DigitOperationsApp.java
+++ src/main/java/lesson8/DigitOperationsApp.java
@@ -11,16 +11,16 @@ public class DigitOperationsApp {
         int prod = 1;
         int count = 0;
 
-        while(num >0 ){
-            int rem = num %10;
+        while (num > 0) {
+            int rem = num % 10;
             num /= 10;
             count++;
             sum += rem;
             prod *= rem;
         }
         // 3. output
-        System.out.printf("Sum of digits is: %d\n",sum);
-        System.out.printf("Prod of digits is: %d\n",prod);
-        System.out.printf("Count of digits is:\n",count);
+        System.out.printf("Sum of digits is: %d\n", sum);
+        System.out.printf("Prod of digits is: %d\n", prod);
+        System.out.printf("Count of digits is:\n", count);
     }
 }
