4fe8ddcaf274b7fdfa468597f041ef98f61b864c
farajzadaa <farajzada@icloud.com>
2023-03-14 19:40:05 +0400

    initial commit

---
+++ src/main/java/lesson10/Array.java
@@ -0,0 +1,12 @@
+package lesson10;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+
+        System.out.printf("%.2f",a);
+    }
+}
