b26030d56abc8fa3eea5f9ab609a55d83d2dbd5e
farajzadaa <farajzada@icloud.com>
2023-04-27 20:18:21 +0400

    exceptions app

--- src/main/java/lesson12/ExceptionApp.java
+++ src/main/java/lesson12/ExceptionApp.java
@@ -6,14 +6,14 @@ public class ExceptionApp {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         String scan = a.next();
+        String scan1 = a.next();
 
         try {
-            int scan1 = Integer.parseInt(scan);
-            while (scan1 > 0) {
-                System.out.println(scan1 % 10);
-                scan1 /= 10;
-            }
-        } catch (NumberFormatException e) {
+            int scan2 = Integer.parseInt(scan);
+            int scan3 = Integer.parseInt(scan1);
+            double result = (double) scan2 / scan3;
+            System.out.println(result);
+        } catch (NumberFormatException exception) {
             System.out.println("NumberFormatException");
         }
     }
