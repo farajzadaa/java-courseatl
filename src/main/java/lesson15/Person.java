1c09b2cab297a08e3d549616d177881f24e9561b
farajzadaa <farajzada@icloud.com>
2023-05-08 19:34:02 +0400

    exceptions app

--- src/main/java/lesson15/Person.java
+++ src/main/java/lesson15/Person.java
@@ -1,12 +1,12 @@
 package lesson15;
 
 public final class Person implements Comparable<Person> {
-    public final  int id;
+    public final int id;
     public final double salary;
     public final String name;
-    public  final int age;
+    public final int age;
 
-    public Person(int id, int age, String name,double salary) {
+    public Person(int id, int age, String name, double salary) {
         this.id = id;
         this.name = name;
         this.age = age;
@@ -16,12 +16,12 @@ public final class Person implements Comparable<Person> {
 
     @Override
     public String toString() {
-        return String.format("Student{id=%d, salary=%s, name='%s',age=%s}", id, salary, name,age);
+        return String.format("Student{id=%d, salary=%s, name='%s',age=%s}", id, salary, name, age);
     }
 
 
     @Override
     public int compareTo(Person that) {
-        return Integer.compare(this.id,that.id);
+        return Integer.compare(this.id, that.id);
     }
 }
--- src/main/java/lesson15/PersonApp.java
+++ src/main/java/lesson15/PersonApp.java
@@ -6,10 +6,10 @@ import java.util.Comparator;
 public class PersonApp {
     public static void main(String[] args) {
         Person[] person = {
-                new Person(1,28,"Samir",1500),
-                new Person(2,24,"Elvin",15000),
-                new Person(3,18,"Murad",1500000),
-                new Person(4,49,"Filankes",15)
+                new Person(1, 28, "Samir", 1500),
+                new Person(2, 24, "Elvin", 15000),
+                new Person(3, 18, "Murad", 1500000),
+                new Person(4, 49, "Filankes", 15)
 
         };
 
@@ -19,11 +19,11 @@ public class PersonApp {
         Arrays.sort(person, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
-                return Integer.compare(o2.age,o1.age);
+                return Integer.compare(o2.age, o1.age);
             }
         });
         System.out.println(Arrays.toString(person));
-        Arrays.sort(person,(person1,person2) ->(Double.compare(person1.salary,person2.salary)));
+        Arrays.sort(person, (person1, person2) -> (Double.compare(person1.salary, person2.salary)));
         System.out.println(Arrays.toString(person));
 
     }
